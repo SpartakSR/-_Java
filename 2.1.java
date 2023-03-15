0cab97eb517d9f068af69eaee8aad881ffc5546f
Сабирьянов Спартак <sabiryanov_s@mail.ru>
2023-03-15 22:37:23 +0300

    Create 2.1.java

---
+++ 2.1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+   static Scanner sc = new Scanner(System.in);
+   public static void main(String args[])
+   {
+      System.out.print("Введите радиус: ");
+      double radius = sc.nextDouble();
+      double area = Math.round(Math.PI * (radius * radius));
+      System.out.println("Площадь круга равна: " + area);
+   }
+}
